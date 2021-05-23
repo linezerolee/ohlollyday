bd4b72bfe222e969a750110c29973ed0e8bf8485
Sunyoung Lee <sunyounglee@Sunyoungui-iMac.local>
2021-05-23 17:37:00 +0900

    최종1
    반응형, 제이쿼리

--- js/common.js
+++ js/common.js
@@ -1,13 +1,40 @@
 $(function(){
     const addTop = 150;
+    let sectionAbout = $('#about');
 
-$("nav ul li a,.back_to_top a,a.button").click(function(){
+$("nav ul li a,.back_to_top a,a").click(function(){
     console.log($(this.hash))
     let thisElem = $(this.hash);
     let offsetElem = thisElem.offset();
     console.log(offsetElem.top)
     $("html,body").stop();
-    $("html,body").animate({scrollTop : offsetElem.top},1500);
+    $("html,body").animate({scrollTop : offsetElem.top},1200);
     });
 
+    //테블릿 화면 메뉴 버튼 효과
+    $(".toggle_btn").click(function(){
+        $("#gnb ul").toggle('open');
+        });
+    
+    //스크롤 시 애니메이션 작동
+    let wHeight = $(window).innerHeight();
+    console.log("window height: "+wHeight)
+    $(window).scroll(function(){
+        let thisScrollTop = $(this).scrollTop();
+        $("section").each(function(){
+        let thisOffset = $(this).offset();
+        if( thisOffset.top <= thisScrollTop + addTop && thisScrollTop < thisOffset.top + wHeight ){
+            $(this).addClass("active");
+        }
+        });
+    });
+        //go_to_top 버튼 애니메이션 효과
+        $(window).scroll(function(){
+        console.log($(this).scrollTop())
+        if($(this).scrollTop() == 0){
+            $(".go_to_top").removeClass("on")
+        }else{
+            $(".go_to_top").addClass("on")
+        }
+        });
 });
