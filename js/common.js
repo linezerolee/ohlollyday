6c05550993175efd71e3ad2c01c7653cfa0103a4
Sunyoung Lee <sunyounglee@Sunyoungui-iMac.local>
2021-05-24 19:57:14 +0900

    스크롤 효과 완성 최종

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(function(){
     const addTop = 150;
-
-    $("nav ul li a,.back_to_top a,a").click(function(){
+    //클릭 시, 해당 섹션 이동
+    $("nav ul li a, .go_to_top a, .button").click(function(){
         //console.log($(this.hash))
         let thisElem = $(this.hash);
         let offsetElem = thisElem.offset();
@@ -9,6 +9,19 @@ $(function(){
         $("html,body").stop();
         $("html,body").animate({scrollTop : offsetElem.top},1200);
         });
+
+    //gnb 클릭 시, 호버 효과 고정 
+    let menu = $('#gnb ul li')
+    let contents = $('section');
+    menu.click(function(x){
+        x.preventDefault();
+        $(this).addClass('menuline').siblings().removeClass('menuline');
+        let idx = $(this).index();
+        let section = contents.eq(idx);
+        let sectionDistance = section.offset().top;
+        $('html,body').stop().animate({scrollTop:sectionDistance},1200);
+    });
+
 
     //테블릿 화면 메뉴 버튼 효과
     $(".toggle_btn").click(function(){
@@ -48,4 +61,59 @@ $(function(){
             $(".go_to_top").addClass("on")
         }
         });
+});
+
+//자바스크립트 //스크롤 이동 시, 메뉴 호버 효과 이동
+'use strict';
+
+const sectionIds = [
+    '#intro',
+    '#about',
+    '#bestseller',
+    '#products',
+    '#stroy',
+    '#campaign',
+    '#issue',
+    '#footer',
+];
+
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected){
+    selectedNavItem.classList.remove('menuline');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('menuline');
+}
+const observerOptions = {
+    root : null,
+    rootMargin : '0px',
+    threshold : .3,
+}
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            if(entry.boundingClientRect.y < 0){
+                selectedNavIndex = index + 1;
+            }else {
+                selectedNavIndex = index - 1;
+            }
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', ()=> {
+    if(window.scrollY === 0){
+        selectedNavIndex = 0;
+    }else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight){
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
 });
